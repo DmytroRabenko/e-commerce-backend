b5337f2d85303ef69100acd09d0532a171376e62
Dmytro Rabenko <rabenkod@ukr.net>
2024-03-20 18:57:36 +0200

    test

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-//  об'єднуємо всі роути в одному файлі
-const Router = require('express')
-const router = new Router()
-const categoryRouter = require('./category.router')
-
-
-router.use('/category', categoryRouter)
-
-module.exports = router
