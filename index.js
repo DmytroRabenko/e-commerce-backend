789a7311e3dcec8ff4711fc684bc6cebb79dc680
Dmytro Rabenko <rabenkod@ukr.net>
2024-03-20 19:41:29 +0200

    no message

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require("express");
-require("dotenv").config();
+const express = require("express")
+const cors = require('cors')
+require("dotenv").config()
 //роути
 const categoryRouter = require('./routes/category.router')
+
 const port = process.env.PORT || 3001;
 
 
 const app = express();
 
 
+app.use(cors())
 app.use(express.json());
 app.use('/api', categoryRouter)
 
@@ -15,7 +18,9 @@ app.use('/api', categoryRouter)
 app.get("/", (req, res) => {
   res.send("сервер працює за запитом /");
 });
-
+app.get("/about", (req, res) => {
+  res.send("сервер працює за запитом /about");
+});
 app.listen(port, () => {
   console.log(`Server listening on ${port}`);
 });
